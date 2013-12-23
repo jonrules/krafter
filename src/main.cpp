67fa5476001c601b499d69a992728f04543eb9de
Jonathan Baltazar <jonrules@gmail.com>
2013-12-22 21:42:38 -0600

    Added src folder and main function.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 22, 2013
+ *      Author: Jonathan Baltazar
+ */
+
+#include <iostream>
+
+int main(int argc, const char *argv[]) {
+	std::cout << "Content-Type: text/plain\r\n";
+	std::cout << "\r\n";
+	std::cout << "Hello!\n";
+	return 0;
+}
+
+
